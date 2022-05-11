4fd9094948915b0ce4e255b53e7788fe603acd7a
HansikaPerera1117 <hansikaperera59@gmail.com>
2022-05-11 20:57:07 +0530

    refactor queryDaoImpl and QueryDAO to set join querys

--- Layered_Architecture/src/controller/PlaceOrderFormController.java
+++ Layered_Architecture/src/controller/PlaceOrderFormController.java
@@ -3,14 +3,8 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
-import dao.custom.CustomerDAO;
-import dao.custom.ItemDAO;
-import dao.custom.OrderDAO;
-import dao.custom.OrderDetailDAO;
-import dao.custom.impl.CustomerDAOImpl;
-import dao.custom.impl.ItemDAOImpl;
-import dao.custom.impl.OrderDAOImpl;
-import dao.custom.impl.OrderDetailDAOImpl;
+import dao.custom.*;
+import dao.custom.impl.*;
 import db.DBConnection;
 import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyObjectWrapper;
@@ -65,6 +59,8 @@ public class PlaceOrderFormController {
     private ItemDAO itemDAO = new ItemDAOImpl();
     private OrderDAO orderDTO = new OrderDAOImpl();
     private OrderDetailDAO orderDetailsDAO = new OrderDetailDAOImpl();
+    private QueryDAO queryDAO = new QueryDAOImpl();
+
 
     public void initialize() throws SQLException, ClassNotFoundException {
 
